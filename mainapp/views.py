cb3904c83f3122a7daa082b8ca934d14a10512f4
lst369 <work7onov@gmail.com>
2022-11-21 13:22:57 +0300

    Passing data to templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    data = {
+        'title': 'Главная страница ЛСТ',
+        'values': ['some', 'hello', 'my name is ЛСТ'],
+        'obj':{
+            'bike': 'BMW',
+            'age': '25',
+            'hobby': 'growing',
+        }
+    }
+    return render(request, 'mainapp/index.html', data )
 
 def about(request):
     return render(request, 'mainapp/about.html')
